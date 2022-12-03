f14bba746371aae1481ed6d993a3db4dc0ff2e2f
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2022-12-03 21:19:50 +0900

    Update the package system

---
+++ a.cbl
@@ -0,0 +1,8 @@
+       identification division.
+       program-id. a.
+       procedure division.
+       evaluate 1 also 2
+       when 1 also 2
+         go to aa
+       end-evaluate.
+       aa.
